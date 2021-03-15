cd5e2c21c277f8fdfc141959a06a7994d21e8b88
Gregory Mundy <gregmundy@gmail.com>
2021-03-15 17:15:03 -0400

    chore: complete deployment with infrastructure

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -6,4 +6,4 @@ export default function main(app) {
   new DynamoDBStack(app, 'dynamodb');
   const s3 = new S3Stack(app, 's3');
   new CognitoStack(app, 'cognito', { bucketArn: s3.bucket.bucketArn });
-}`1`
+}
