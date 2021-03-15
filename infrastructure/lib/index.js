b3e4e77bc2ae2cb2c87f02961ef61075549eece5
Gregory Mundy <gregmundy@gmail.com>
2021-03-15 15:34:53 -0400

    chore: add lib from serverless

---
+++ infrastructure/lib/index.js
@@ -0,0 +1,9 @@
+import DynamoDBStack from './DynamoDBStack';
+import S3Stack from './S3Stack';
+import CognitoStack from './CognitoStack';
+
+export default function main(app) {
+  new DynamoDBStack(app, 'dynamodb');
+  const s3 = new S3Stack(app, 's3');
+  new CognitoStack(app, 'cognito', { bucketArn: s3.bucket.bucketArn });
+}`1`
